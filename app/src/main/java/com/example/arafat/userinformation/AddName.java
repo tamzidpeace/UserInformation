55fc706e3a3689fe7d5ce8d61b47ebb152bfe669
techdroweb <45241872+techdroweb@users.noreply.github.com>
2018-12-11 00:55:46 +0600

    second installment

--- app/src/main/java/com/example/arafat/userinformation/AddName.java
+++ app/src/main/java/com/example/arafat/userinformation/AddName.java
@@ -1,13 +1,36 @@
 package com.example.arafat.userinformation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddName extends AppCompatActivity {
+
+    EditText firstName, lastName, mEmail;
+    private Button mSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_name);
+
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        mEmail = findViewById(R.id.email);
+        mSave = findViewById(R.id.save);
+
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String catchFirstName = firstName.getText().toString();
+                String catchLastName = lastName.getText().toString();
+                String catchEmail = mEmail.getText().toString();
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
