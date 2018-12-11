180144f1cc25ce8c5cff7743a93187f9184eb641
techdroweb <45241872+techdroweb@users.noreply.github.com>
2018-12-11 10:50:27 +0600

    Setting up RecyclerView

--- app/src/main/java/com/example/arafat/userinformation/AddName.java
+++ app/src/main/java/com/example/arafat/userinformation/AddName.java
@@ -3,6 +3,7 @@ package com.example.arafat.userinformation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,8 @@ public class AddName extends AppCompatActivity {
 
     EditText firstName, lastName, mEmail;
     private Button mSave;
+    private static final String TAG = "AddName";
+    private static final String TAG1 = "testing error";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class AddName extends AppCompatActivity {
                 String catchLastName = lastName.getText().toString();
                 String catchEmail = mEmail.getText().toString();
 
+                Log.d(TAG, catchFirstName + " " + catchLastName + " " + catchEmail);
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
--- app/src/main/java/com/example/arafat/userinformation/MainActivity.java
+++ app/src/main/java/com/example/arafat/userinformation/MainActivity.java
@@ -3,22 +3,45 @@ package com.example.arafat.userinformation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
     Button addName;
+    private static final String TAG = "MainActivity";
+    private RecyclerView recyclerView;
+    private List<String> name = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //todo work with adapter
+        for (int i = 0; i <10 ; i++) {
+            name.add("arafat " + i);
+        }
+        recyclerView.setAdapter(new NameAdapter(name));
 
         addName = findViewById(R.id.add_name);
 
         addName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // TODO: 11-Dec-18  : use of Log-d
+                Log.d(TAG, "clicked");
                 startActivity(new  Intent(MainActivity.this, AddName.class));
             }
         });
